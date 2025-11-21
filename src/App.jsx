384b57affd66593697d45a22504cdf58748c83ad
KhodaniD <khodanid@gmail.com>
2025-11-21 10:05:45 +0200

    Move context provider stack to main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,47 +6,30 @@ import Home from "./pages/Home.jsx";
 import ShowDetail from "./pages/ShowDetail.jsx";
 import FavouritesPage from "./pages/FavouritesPage.jsx";
 
-// Context Providers (imported as default or named exports based on context definitions)
-import PodcastProvider from "./context/PodcastContext.jsx";
-import { ThemeProvider } from "./context/ThemeContext.jsx";
-import FavouritesProvider from "./context/FavouritesContext.jsx";
-import { AudioPlayerProvider } from "./context/AudioPlayerContext.jsx";
-import { HistoryProvider } from "./context/HistoryContext.jsx";
-
 import AudioPlayer from "./components/AudioPlayer/AudioPlayer.jsx";
 import "./global.css";
 
 /**
- * Root component of the Podcast Explorer app.
- * Defines the entire application architecture: Context stack, routing, and persistent UI elements.
+ * Root component of the application.
+ * Defines the main page layout, navigation, and persistent UI elements (Header, AudioPlayer).
+ * NOTE: All Context Providers are now stacked in the root entry file (main.jsx).
  */
 export default function App() {
   return (
-    // Stack of context providers, ensuring theme is available globally
-    <ThemeProvider>
-        <FavouritesProvider>
-            <HistoryProvider>
-                <AudioPlayerProvider>
-                    <PodcastProvider>
-                        
-                        {/* Header is inside the providers to access search and theme contexts */}
-                        <Header />
-                        
-                        <main>
-                            <Routes>
-                                {/* Main Application Routes */}
-                                <Route path="/" element={<Home />} />
-                                <Route path="/show/:id" element={<ShowDetail />} />
-                                <Route path="/favourites" element={<FavouritesPage />} />
-                            </Routes>
-                        </main>
-                        
-                        {/* Persistent Audio Player is displayed at the bottom of all views */}
-                        <AudioPlayer />
-                    </PodcastProvider>
-                </AudioPlayerProvider>
-            </HistoryProvider>
-        </FavouritesProvider>
-    </ThemeProvider>
+    <>
+      <Header />
+      
+      <main>
+        <Routes>
+          {/* Main Application Routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/show/:id" element={<ShowDetail />} />
+          <Route path="/favourites" element={<FavouritesPage />} />
+        </Routes>
+      </main>
+      
+      {/* Persistent Audio Player */}
+      <AudioPlayer />
+    </>
   );
 }
