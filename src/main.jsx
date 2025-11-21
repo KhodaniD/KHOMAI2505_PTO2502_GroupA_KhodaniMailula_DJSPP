d9242f3f1fefd0afaa5c6fbc8fd10882a14697ad
KhodaniD <khodanid@gmail.com>
2025-11-21 10:03:30 +0200

    Finalize root rendering with router and context stack

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Context Providers
+import { ThemeProvider } from './context/ThemeContext.jsx';
+import PodcastProvider from './context/PodcastContext.jsx';
+import FavouritesProvider from './context/FavouritesContext.jsx';
+import { AudioPlayerProvider } from './context/AudioPlayerContext.jsx';
+import { HistoryProvider } from './context/HistoryContext.jsx';
+
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      {/* BrowserRouter wraps the entire application to enable client-side routing */}
+      <BrowserRouter>
+        {/* Stack all Context Providers to ensure state is available globally */}
+        <ThemeProvider>
+          <FavouritesProvider>
+            <HistoryProvider>
+              <AudioPlayerProvider>
+                <PodcastProvider>
+                  <App />
+                </PodcastProvider>
+              </AudioPlayerProvider>
+            </HistoryProvider>
+          </FavouritesProvider>
+        </ThemeProvider>
+      </BrowserRouter>
+    </StrictMode>,
+  );
+}
